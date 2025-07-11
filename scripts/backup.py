71284a57d1d82e6a6dfd0f363227a879ea6f270a
secretgammaalpha <gammaraytwitch@gmail.com>
2025-07-11 12:30:23 +0100

    2.14

--- scripts/backup.py
+++ scripts/backup.py
@@ -13,25 +13,25 @@ class backup():
         Install_Dir = content["default_settings"]["INSTALL_DIR"]
         filename = content["user_settings"]["file_name"]
     def decompress():
-        os.rename(f'{Install_Dir}/Terminal---2.01/{filename}.docx',f'{Install_Dir}/Terminal---2.01/{filename}.zip')
+        os.rename(f'{Install_Dir}/Terminal---2.02/{filename}.docx',f'{Install_Dir}/Terminal---2.02/{filename}.zip')
         time.sleep(3)
-        shutil.unpack_archive(f'{Install_Dir}/Terminal---2.01/{filename}.zip','C:/Windows/Temp/temp_PasswdManager1.1/')
+        shutil.unpack_archive(f'{Install_Dir}/Terminal---2.02/{filename}.zip','C:/Windows/Temp/temp_PasswdManager1.1/')
     def compress():
         backup.decompress()
         shutil.copy('Password.json', 'C:/Windows/Temp/temp_PasswdManager1.1/')
-        shutil.make_archive(f'{Install_Dir}/Terminal---2.01/{filename}','zip','C:/Windows/Temp/temp_PasswdManager1.1/')
-        os.rename(f'{Install_Dir}/Terminal---2.01/{filename}.zip',f'{Install_Dir}/Terminal---2.01/{filename}.docx')
+        shutil.make_archive(f'{Install_Dir}/Terminal---2.02/{filename}','zip','C:/Windows/Temp/temp_PasswdManager1.1/')
+        os.rename(f'{Install_Dir}/Terminal---2.02/{filename}.zip',f'{Install_Dir}/Terminal---2.02/{filename}.docx')
         shutil.rmtree('C:/Windows/Temp/temp_PasswdManager1.1/')
         print('backup is Done!')
     def on_start_decompress():
-        os.rename(f'{Install_Dir}/Terminal---2.01/{filename}.docx',f'{Install_Dir}/Terminal---2.01/{filename}.zip')
+        os.rename(f'{Install_Dir}/Terminal---2.02/{filename}.docx',f'{Install_Dir}/Terminal---2.02/{filename}.zip')
         print('unpacking...')
-        shutil.unpack_archive(f'{Install_Dir}/Terminal---2.01/{filename}.zip','C:/Windows/Temp/temp_PasswdManager1.1/')
+        shutil.unpack_archive(f'{Install_Dir}/Terminal---2.02/{filename}.zip','C:/Windows/Temp/temp_PasswdManager1.1/')
         time.sleep(2)
         print('copying...')
         shutil.copy('C:/Windows/Temp/temp_PasswdManager1.1/Password.json','C:/Users/Admin/Documents/PythonProjects/PasswordManager2.0/')
         time.sleep(2)
-        os.rename(f'{Install_Dir}/Terminal---2.01/{filename}.zip',f'{Install_Dir}/Terminal---2.01/{filename}.docx')
+        os.rename(f'{Install_Dir}/Terminal---2.02/{filename}.zip',f'{Install_Dir}/Terminal---2.02/{filename}.docx')
         print('cleaning...')
         shutil.rmtree('C:/Windows/Temp/temp_PasswdManager1.1/')
         time.sleep(2)
